65b1a4d0997f49845d3153026990c6ff513c270b
Administrator <szyyyp@163.com>
2023-04-07 23:59:16 +0800

    2023-4-7 szy 修改： 1 净化了框架代码

--- src/main/java/org/example/controller/ScaleInfoController.java
+++ src/main/java/org/example/controller/ScaleInfoController.java
@@ -22,62 +22,11 @@ public class ScaleInfoController {
     @Resource(name="scaleServiceImpl")
     ScaleService scaleService;
 
-/*    @Resource(name="canMeterServiceImpl")
-    CanMeterService canMeterService;*/
-
     // 获取称台参数
     @RequestMapping("/getScaleInfo")
     public Page<Scale> getScaleInfo(Pageable page, Scale scale){
 
         return scaleService.findPage(page,scale);
-    }
-
-    @RequestMapping("/scaleUp")
-    public Json scaleUp(Integer id){
-        Json json = new Json();
-        try {
-/*            PLCCmd cmd = plcCmdService.getSendToPlcCmd(id + 100, PlcCmdSendValue.SCALE_JACKING.getCode());
-            plcClient.sendToPlc(cmd);*/
-            json.setSuccess(true);
-            json.setMsg("电钢顶升指令发送成功!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            json.setSuccess(true);
-            json.setMsg("电钢顶升指令发送失败");
-        }
-        return json;
-    }
-
-    @RequestMapping("/scaleDown")
-    public Json scaleDown(Integer id){
-        Json json = new Json();
-        try {
-/*            PLCCmd cmd = plcCmdService.getSendToPlcCmd(id + 100, PlcCmdSendValue.SCALE_DOWN.getCode());
-            plcClient.sendToPlc(cmd);*/
-            json.setSuccess(true);
-            json.setMsg("电钢下降指令发送成功!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            json.setSuccess(true);
-            json.setMsg("电钢下降指令发送失败");
-        }
-        return json;
-    }
-
-    @RequestMapping("/removeFaultBell")
-    public Json removeFaultBell(Integer id){
-        Json json = new Json();
-        try {
-/*            PLCCmd cmd = plcCmdService.getSendToPlcCmd(id + 100, PlcCmdSendValue.REMOVE_ERROR.getCode());
-            plcClient.sendToPlc(cmd);*/
-            json.setSuccess(true);
-            json.setMsg("取消故障告警指令发送成功!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            json.setSuccess(true);
-            json.setMsg("取消故障告警指令发送失败");
-        }
-        return json;
     }
 
     @RequestMapping("/edit")
@@ -126,16 +75,5 @@ public class ScaleInfoController {
         }
         return json;
     }
-
-
-
-
-
-/*    public static void main(String [] args){
-        int k =(int) Math.ceil(0/2d);
-        int j = 0/2;
-        System.out.println(k);
-        System.out.println(j);
-    }*/
 
 }
